9a9ac280fb561455d051de0babc2de79acaa0dbb
EugeneSchepard <schepa.evgen@gmail.com>
2017-07-02 01:03:56 +0300

    Task 1 is completed

--- src/test/java/com/FirstTask/app/MvcTest.java
+++ src/test/java/com/FirstTask/app/MvcTest.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 
 /**
- * Created by Laptop on 27.06.2017.
+ * Some experiments with CSS selectors;
  */
 public class MvcTest {
 
@@ -17,8 +17,8 @@ public class MvcTest {
         $("#new-todo").shouldBe(visible).setValue("a").pressEnter();
         $("#new-todo").setValue("b").pressEnter();
         $("#new-todo").setValue("c").pressEnter();
-
         $$("#todo-list>li").shouldHave(exactTexts("a","b","c"));
+
         $("#todo-list>li:nth-child(2)>div>.toggle").click();
         $("#todo-list>li:nth-child(2)").shouldHave(cssClass("completed"));
         $("#todo-list>li:nth-child(1):not(.completed)");
--- src/test/java/com/FirstTask/app/MvcTest1.java
+++ src/test/java/com/FirstTask/app/MvcTest1.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 
 /**
- * Created by Laptop on 27.06.2017.
+ * Some experiments with Xpath selectors;
  */
 public class MvcTest1 {
 
@@ -17,15 +17,14 @@ public class MvcTest1 {
         $x("//*[@id='new-todo']").shouldBe(visible).setValue("a").pressEnter();
         $x("//*[@id='new-todo']").setValue("b").pressEnter();
         $x("//*[@id='new-todo']").setValue("c").pressEnter();
-
-        $x("//*//label[text()='v']");
+        $x("//*//label[text()='a']");
         $x("//*//label[text()[contains (.,'b')]]");
         $x("//div[@id='ember225']//section[@id='main']/ul/li//label[text()[contains(.,'c')]]");
 
         $x("//*//label[text()='b']/../input[@class='toggle']").click();
         $x("//*[@id='todo-list']/li[contains (@class, 'completed')]").shouldHave(exactText("b"));
-        $x("//*//li[@class=\"ember-view\"]//label[text()[contains (.,\"a\")]]/../input[@class=\"toggle\"]");
-        $x("//*//li[@class=\"ember-view\"]//label[text()[contains (.,\"c\")]]/../input[@class=\"toggle\"]");
+        $x("//*//label[text()[contains (.,'a')]]").shouldNotHave(cssClass("completed"));
+        $x("//*//label[text()[contains (.,'c')]]/../input[not(contains (@class, 'completed'))]");
 
 
 
