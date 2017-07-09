dd77e73995a089f3719f1989415ea34c6a973688
EugeneSchepard <schepa.evgen@gmail.com>
2017-07-10 01:09:29 +0300

    second task

--- src/test/java/firstTask/TodoMvcTest1.java
+++ src/test/java/firstTask/TodoMvcTest1.java
@@ -18,9 +18,12 @@ public class TodoMvcTest1 {
         $x("//*[@id='new-todo']").setValue("a").pressEnter();
         $x("//*[@id='new-todo']").setValue("b").pressEnter();
         $x("//*[@id='new-todo']").setValue("c").pressEnter();
-        $$("//*[@id='todo-list']").shouldHave(exactTexts("a", "b", "c"));
-        $x("//*[.//*[contains(text(),'b')]]//*[@class='toggle']").click();
-        $x("//*[@id='todo-list']/li[contains (@class, 'completed')]").shouldHave(exactText("b"));
-        $$("//*[@id='todo-list']/li[contains (@class, 'active')]").shouldHave(exactTexts("a", "c"));
+        $$x("//*[@id='todo-list']//li").shouldHave(exactTexts("a", "b", "c"));
+        //$x("//*[@id='todo-list']//li[.//*text()='b']//*[@class='toggle']").click();
+        $(byXpath("//*[@id='todo-list']//li[.//text()='b']//*[@class='toggle']")).click();
+        $x("//*[@id='todo-list']//li[contains(@class, 'completed')]").shouldHave(exactText("b"));
+        $$x("//*[@id='todo-list']//li[contains (@class, 'active')]").shouldHave(exactTexts("a", "c"));
+
+
     }
 }
